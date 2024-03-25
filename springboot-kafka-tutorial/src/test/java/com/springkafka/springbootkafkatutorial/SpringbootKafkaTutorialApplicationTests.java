9a2a4c5144a1a90d2ff47bc98bdd609bc98b4186
suneet Agrawal <sumeetagrawal.srm@gmail.com>
2024-03-26 00:44:17 +0530

    first commit

---
+++ springboot-kafka-tutorial/src/test/java/com/springkafka/springbootkafkatutorial/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springkafka.springbootkafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
